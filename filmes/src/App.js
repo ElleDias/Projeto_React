6e2cefb6c84b826a0e8229be0c1b3d0c9a0bbaa7
Danielle Dias <danielle.c.santos9@aluno.senai.br>
2025-04-24 08:38:17 -0300

    feat: subindo a atividade inicial de react

---
+++ filmes/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CadastroFilme from './pages/cadastroFilme/CadastroFilme.jsx';
+import Login from  "./pages/login/Login.jsx";
+
+function App() {
+  return (
+  <>
+  <CadastroFilme/>
+  </>
+  );
+}
+//qualquer um pode acessar o app, por causa do comando abaixo
+export default App;
---
+++ filmes/src/components/Botao/Botao.jsx
@@ -0,0 +1,7 @@
+import "./Botao.css"
+const Botao = () => {
+    return(
+        <button className="botao">Entrar</button>
+    )
+}
+export default Botao;
